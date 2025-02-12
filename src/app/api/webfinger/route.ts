1ea0dd68f08832a1ab6c78d51b61a7590d9f1829
Tyler Holewinski <tyler@holewinski.dev>
2025-02-12 01:50:54 -0500

    fix(site): make webfinger just redirect

--- src/app/api/webfinger/route.ts
+++ src/app/api/webfinger/route.ts
@@ -1,39 +1,19 @@
 import { NextResponse } from "next/server";
 
-export function GET() {
-  return new NextResponse(
-    JSON.stringify({
-      subject: "acct:tyler@social.holewinski.dev",
-      aliases: [
-        "https://social.holewinski.dev/@tyler",
-        "https://social.holewinski.dev/users/tyler",
-      ],
-      links: [
-        {
-          rel: "http://webfinger.net/rel/profile-page",
-          type: "text/html",
-          href: "https://social.holewinski.dev/@tyler",
-        },
-        {
-          rel: "self",
-          type: "application/activity+json",
-          href: "https://social.holewinski.dev/users/tyler",
-        },
-        {
-          rel: "http://ostatus.org/schema/1.0/subscribe",
-          template:
-            "https://social.holewinski.dev/authorize_interaction?uri={uri}",
-        },
-        {
-          rel: "http://webfinger.net/rel/avatar",
-          type: "image/jpeg",
-          href: "https://social.holewinski.dev/system/accounts/avatars/113/936/384/323/503/767/original/c4a58feb74375052.jpg",
-        },
-      ],
-    }),
-    {
-      status: 200,
-      headers: { "Content-Type": "application/jrd+json" },
-    }
-  );
+export function GET(req: Request) {
+  const url = new URL(req.url);
+  const resource = url.searchParams.get("resource");
+
+  const aliases = [
+    "tyler@holewinski.dev",
+    "tyler@social.holewinski.dev",
+    "holewinski.dev",
+  ].map((it) => `acct:${it}`);
+
+  if (!resource || aliases.includes(resource))
+    return NextResponse.redirect(
+      "https://social.holewinski.dev/.well-known/webfinger/?resource=acct:tyler@social.holewinski.dev"
+    );
+
+  return NextResponse.json({ error: "Not found" }, { status: 404 });
 }
