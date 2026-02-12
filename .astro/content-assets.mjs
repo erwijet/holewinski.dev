a879031dabddce4746bde122c0ccbf137f65aac1
Tyler Holewinski <tyler@holewinski.dev>
2026-02-12 00:41:34 -0500

    ignore .astro

--- .astro/content-assets.mjs
+++
@@ -1,5 +0,0 @@
-
-import __ASTRO_IMAGE_IMPORT_1AzeCO from "../../assets/images/better-blockly-fig-1.png?astroContentImageFlag=&importer=src%2Fdata%2Fposts%2Fbetter-blockly.md";
-import __ASTRO_IMAGE_IMPORT_ZeMK2O from "../../assets/images/spotipub-widget.png?astroContentImageFlag=&importer=src%2Fdata%2Fposts%2Fspotipub.md";
-export default new Map([["../../assets/images/better-blockly-fig-1.png?astroContentImageFlag=&importer=src%2Fdata%2Fposts%2Fbetter-blockly.md", __ASTRO_IMAGE_IMPORT_1AzeCO], ["../../assets/images/spotipub-widget.png?astroContentImageFlag=&importer=src%2Fdata%2Fposts%2Fspotipub.md", __ASTRO_IMAGE_IMPORT_ZeMK2O]]);
-		
